440de91cb5956aa47038c4e71e06aed5a24319f9
nickhallph <nick.hall@yum.com>
2018-03-05 13:34:26 -0600

    wip commit

--- tcp/tcp_client.py
+++ tcp/tcp_client.py
@@ -1,8 +1,32 @@
 import socket
 import sys
 
-def Main():
+if not len(sys.argv) == 3:
+    print('Please enter a host and port number')
+    print('USAGE: python ./tcp_client.py 127.0.0.1 4532')
+    quit()
+else:
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    if port <= 1024:
+        print('You must enter a port number above 1024')
+        quit()
+    print('file:', sys.argv[0], 'host:', host, 'port:', port)
 
+def Main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+
+    while True:
+        msg = input('WTF BRO THIS IS FRUSTRATING UGHH')
+        client_socket.sendall(msg)
+
+        data = client_socket.recv(1024)
+        formatted_data = data.format(data)
+        print(str(data))
+
+    client_socket.close()
+    quit()
 
 if __name__ == '__main__':
     Main()
--- tcp/tcp_server.py
+++ tcp/tcp_server.py
@@ -3,7 +3,7 @@ import sys
 
 if not len(sys.argv) == 3:
     print('Please enter a host and port number')
-    print('USAGE: python ./tcp_server.py 127.0.0.1 444')
+    print('USAGE: python ./tcp_server.py 127.0.0.1 4532')
     quit()
 else:
     host = sys.argv[1]
@@ -23,11 +23,29 @@ def Main():
     # Listen for connections
     server_socket.listen(1)
 
-    # Let user know we are listening for a connection
-    print('Listening for connection on port ' + str(port))
+    while True:
+        # Let user know we are listening for a connection
+        print('Listening for connection on port ' + str(port))
 
-    # Accept connection and data from client
-    connection, address = server_socket.accept()
+        # Accept connection and data from client
+        connection, address = server_socket.accept()
+
+        try:
+            print('Connection from: ' + str(address))
+
+            while True:
+                data = connection.recv(1024)
+                print('Received {!r}'.format(data))
+                if data:
+                    print('Sending data to client....')
+                    connection.sendall(data)
+                else:
+                    print('No more data.')
+                    break
+
+        finally:
+            # Close the connection
+            connection.close()
 
 
 if __name__ == '__main__':
